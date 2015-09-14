777cac53ca187b4c497f0f074ce051fdf1150334
Allen Wyma <allen.wyma@gmail.com>
2015-09-14 15:19:46 +0800

    finally caught an edge case, will work on this before merging in

--- bowlingscorer.rb
+++ bowlingscorer.rb
@@ -5,9 +5,10 @@ class BowlingScorer
     numbered_scores.each_with_index do |score, i|
       if i+2 < numbered_scores.length and score == 10 # if it's a strike, get the next two ahead
         new_scores << (score + numbered_scores[i+1] + numbered_scores[i+2])
-      elsif i+2 < numbered_scores.length and score + numbered_scores[+1] == 10 # if it's a spare
+        # if it's a spare, just add the second score, cause you only get the second score from a spare
+      elsif i+2 < numbered_scores.length and score + numbered_scores[+1] == 10 
         new_scores << (score + numbered_scores[i+2])
-      elsif score != 10
+      elsif score != 10 # otherwise, just add the number
         new_scores << score
       end
     end
--- spec.rb
+++ spec.rb
@@ -11,4 +11,16 @@ describe BowlingScorer do
   it 'returns 48 when passed 9,1,10,8,0,2' do
     BowlingScorer.score('9,1,10,8,0,2').must_equal 48
   end
+  it 'returns 50 when passed 10,0,0,9,1,0,0,8,2,0,0,7,3,0,0,6,4,0,0' do
+    BowlingScorer.score('10,0,0,9,1,0,0,8,2,0,0,7,3,0,0,6,4,0,0').must_equal 50
+  end
+  it 'returns 83 when passed 4,6,3,1,9,1,10,7,3,2,3,4,5,10,2' do
+    BowlingScorer.score('4,6,3,1,9,1,10,7,3,2,3,4,5,10,2').must_equal 83
+  end
+  it 'returns 12 when passed 1,2,8,1' do
+    BowlingScorer.score('1,2,8,1').must_equal 12
+  end
+  it 'returns 21 when passed 1,2,8,1,9,0' do
+    BowlingScorer.score('1,2,8,1,9,0').must_equal 21
+  end
 end
