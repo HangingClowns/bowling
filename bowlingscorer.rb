6da7fb6b365a9cf455a1857560e86d10ef549b82
Allen Wyma <allen.wyma@gmail.com>
2015-09-14 14:54:04 +0800

    got 2 more cases working

--- bowlingscorer.rb
+++ bowlingscorer.rb
@@ -3,8 +3,12 @@ class BowlingScorer
     numbered_scores = scores.split(',').map(&:to_i)
     new_scores = []
     numbered_scores.each_with_index do |score, i|
-      if score == 10 and i+2 < numbered_scores.length # if it's a strike, get the next two ahead
+      if i+2 < numbered_scores.length and score == 10 # if it's a strike, get the next two ahead
         new_scores << (score + numbered_scores[i+1] + numbered_scores[i+2])
+      elsif i+2 < numbered_scores.length and score + numbered_scores[+1] == 10 # if it's a spare
+        new_scores << (score + numbered_scores[i+2])
+      elsif score != 10
+        new_scores << score
       end
     end
     new_scores.inject :+
--- spec.rb
+++ spec.rb
@@ -5,4 +5,10 @@ describe BowlingScorer do
   it 'returns 300 when passed a perfect game' do
     BowlingScorer.score('10,10,10,10,10,10,10,10,10,10,10,10').must_equal 300
   end
+  it 'returns 20 when passed 1,2,3,4,5,5' do
+    BowlingScorer.score('1,2,3,4,5,5').must_equal 20
+  end
+  it 'returns 48 when passed 9,1,10,8,0,2' do
+    BowlingScorer.score('9,1,10,8,0,2').must_equal 48
+  end
 end
