ad481bcae7d6254f910637aaacfe7facc5d2b2b0
Allen Wyma <allen.wyma@gmail.com>
2015-09-14 14:32:14 +0800

    got perfect score PASSING

--- bowlingscorer.rb
+++ bowlingscorer.rb
@@ -1,5 +1,12 @@
 class BowlingScorer
   def self.score scores
-    scores.split(',').map(&:to_i).inject(:+)
+    numbered_scores = scores.split(',').map(&:to_i)
+    new_scores = []
+    numbered_scores.each_with_index do |score, i|
+      if score == 10 and i+2 < numbered_scores.length # if it's a strike, get the next two ahead
+        new_scores << (score + numbered_scores[i+1] + numbered_scores[i+2])
+      end
+    end
+    new_scores.inject :+
   end
 end
